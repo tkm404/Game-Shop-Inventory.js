727a2492e7efcb3a773cb2ffe91c82b38384c2b6
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-06-30 09:21:20 -0700

    add Header.js and StoreFrontControl

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header(){
+  const headerStyles = {
+    color: 'black'
+  }
+  return (
+      <div style={headerStyles}>
+    <h1>SillyStringz Silly Games!</h1>
+  </div>
+  )
+
+}
+
+export default Header
---
+++ src/components/StoreFrontControl.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class StoreFrontControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return(
+      <React.Fragment>
+
+      </React.Fragment>
+    )
+  }
+}
+
+export default StoreFrontControl;
