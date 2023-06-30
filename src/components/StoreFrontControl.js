fc43485f7ae1398e600df18216314b81e8077fd7
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-06-30 09:28:33 -0700

    add SFList.js, update ''Control.js to toggle state

--- src/components/StoreFrontControl.js
+++ src/components/StoreFrontControl.js
@@ -1,15 +1,30 @@
 import React from "react";
+import StoreFrontList from './StoreFrontList'
 
 class StoreFrontControl extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      formVisibleOnPage: false
+    };
+
   }
 
   render(){
+    let currentlyVisibleState = null;
+      if (this.state.formVisibleOnPage) {
+        currentlyVisibleState = <StoreFrontList />
+      } else {
+        currentlyVisibleState = 
+        <React.Fragment>
+          <p>Placeholder</p>
+        </React.Fragment>
+      }
     return(
+      
       <React.Fragment>
+        {currentlyVisibleState}
 
       </React.Fragment>
     )
---
+++ src/components/StoreFrontList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function StoreFrontList(){
+  <React.Fragment>
+    <p>Placeholder</p>
+  </React.Fragment>
+}
+
+export default StoreFrontList;
