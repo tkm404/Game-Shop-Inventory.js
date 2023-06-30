06f0510526b99d965c4a20cb7a3a0e5b6a24a8a8
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-06-30 11:20:37 -0700

    prepare SFList to take Pallets with properties

--- src/components/StoreFrontList.js
+++ src/components/StoreFrontList.js
@@ -1,12 +1,26 @@
 import React from "react";
+import Pallet from "./Pallet";
+import PropTypes from "prop-types"
+import { v4 } from "uuid";
 
 function StoreFrontList(props){
   return(
       <React.Fragment>
-    <p>Placeholder</p>
+    <hr />
+    {props.palletList.map((pallet) =>
+      <Pallet 
+        name={pallet.name}
+        genre={pallet.genre}
+        price={pallet.price}
+        accolade={pallet.accolade}
+        id={pallet.id} />)}
   </React.Fragment>
   )
 
 }
+
+StoreFrontList.propTypes = {
+  palletList: PropTypes.array
+};
 
 export default StoreFrontList;
