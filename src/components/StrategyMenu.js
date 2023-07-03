41f9277b1217077dbca6f990bad5bbde667c016e
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-07-02 17:47:44 -0700

    add strategy and dex menus. add body to strategy menu

---
+++ src/components/StrategyMenu.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Pallet from "./Pallet";
+import PropTypes from "prop-types"
+
+
+function StrategyMenu(props){
+  return(
+    <React.Fragment>
+  <hr />
+
+  {props.byStrategy.map((pallet) =>
+    <Pallet 
+      name={pallet.name}
+      genre={pallet.genre}
+      price={pallet.price}
+      quantity={pallet.quantity}
+      id={pallet.id}
+      key={pallet.id} />)}
+      <hr />
+</React.Fragment>
+)
+  
+}
+
+StrategyMenu.propTypes = {
+  palletList: PropTypes.array
+};
+
+export default StrategyMenu
